5aea8e075ab21fa3a809dbe8189a0b3d2c9c5f92
Rafael Martins <57511129+rafa7ss@users.noreply.github.com>
2021-04-14 15:58:47 -0300

    Primeiro upload

---
+++ request.py
@@ -0,0 +1,10 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+
+dados = {'Username':'Olivia','Password':'123'}
+req = requests.post('http://201.6.1.17/net2/login.asp',data = dados)
+soup = BeautifulSoup(req.text, 'html.parser')
+print(soup.prettify)
