b9b342193a3b4608c4c451d8aa441e8657446e9f
Rafael Martins <57511129+rafa7ss@users.noreply.github.com>
2021-04-16 17:09:27 -0300

    Login and Download
    Login is working, but the website throws a error sometimes.
    
    Download isn't working for now, but is under development.

---
+++ codigos.py
@@ -0,0 +1,7 @@
+#procura string dentro do código html da página
+'''i = (driver.page_source).find(dados.ofsc)
+    OFSC = ""
+    while driver.page_source[i] != "'":
+        OFSC += driver.page_source[i]
+        i += 1
+        auth = driver.find_element_by_name('AuthState').get_attribute("value")'''
--- crawler_selenium.py
+++ crawler_selenium.py
@@ -1,20 +1,79 @@
 import time
+import dados
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver_path = 'chromedriver'
 options = Options()
-options.headless = True
+options.headless = False
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--window-size=1400,800")
 driver = webdriver.Chrome(options=options, executable_path=driver_path)
-driver.get("https://clarobrasil.etadirect.com/mobility/")
+driver.get(dados.url)
 
-time.sleep(10)
+def login1():
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "username"))
+        )
+    finally:
+        text = driver.find_element_by_xpath('//input[@id="username"]')
+        text.send_keys(dados.usuario)
+        text = driver.find_element_by_xpath('//input[@id="password"]')
+        text.send_keys(dados.senha)
+        button = driver.find_element_by_name('user_submitted_login_form')
+        time.sleep(6)
+        button.click()
+        login2()
 
-i = 2448
-AuthState = ""
-while driver.page_source[i] != "'":
-    AuthState += driver.page_source[i]
-    i += 1
+def login2():
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "username"))
+        )
+    finally:
+        text = driver.find_element_by_name('username')
+        text.send_keys(dados.usuario)
+        text = driver.find_element_by_name('password')
+        text.send_keys(dados.senha)
+        button = driver.find_element_by_class_name('campo-buttom')
+        time.sleep(6)
+        button.click()
+        listarCategorias()
 
-print(AuthState)
-driver.quit()
+def listarCategorias():
+    try:
+        element = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "toa-panel-content edtree"))
+        )
+    finally:
+        div = driver.find_elements_by_class_name("edt-root")
+        for divs in div:
+            if (divs.text).find("BHZ-INFRAREDES") and divs.text != "":
+                div = div[div.index(divs)]
+                break
+        div = div.find_elements_by_class_name("edt-item")
+        for divs in div:
+            divs.click()
+            time.sleep(5)
+        driver.quit()
+        raise SystemExit
+        '''exportar()'''
+
+def exportar():
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "toa-twopanel-first-panel ui-droppable"))
+        )
+    finally:
+        button = driver.find_element_by_class_name('toa-button toa-view-control-button-menu')
+        button.click()
+        time.sleep(1)
+        button = driver.find_element_by_name('export_queue')
+        button.click()
+        time.sleep(1)
+
+login1()
